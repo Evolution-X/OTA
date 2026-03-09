769965188a8a3ec7a728c3a13d2d42a994e6a33b
TechPanelGM <giomook2003@gmail.com>
2026-03-09 11:07:36 +0100

    [WIP] .github: Add simple telegram webhook
    - API will be improved yet with a proper response

--- .github/push.py
+++ .github/push.py
@@ -6,6 +6,7 @@ import json
 
 file_path = sys.argv[1]
 
+telegram_webhook = os.environ["TELEGRAM_WEBHOOK"]
 bka_webhook = os.environ["BKA_WEBHOOK"]
 udc_webhook = os.environ["UDC_WEBHOOK"]
 udc_vanilla_webhook = os.environ["UDC_VANILLA_WEBHOOK"]
@@ -54,7 +55,16 @@ def webhook_send():
     # Extract only the branch name from GITHUB_REF, default to 'vic'
     branch = os.environ.get("GITHUB_REF", "refs/heads/vic").split("/")[-1]
     commit_hash = get_commit_hash(branch, codename)
-    
+
+    # Telegram Hook
+    telegram_url = f"{telegram_webhook}c={codename}&b={branch}"
+
+    try:
+        requests.get(telegram_url, timeout=2)
+    except requests.exceptions.RequestException:
+        pass  # ignore any errors
+
+    # Discord Hook
     if "Vanilla" in filename and "11." in version:
         webhook_url = bka_vanilla_webhook
     if "Vanilla" in filename and "10." in version:
